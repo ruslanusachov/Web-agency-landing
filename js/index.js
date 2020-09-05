bb034905ceba8da766ed2ed7de3e8a62fe1228d2
Ruslan <ruslanusachov2@gmail.com>
2020-09-05 22:18:58 +0300

    made blank gallery

--- js/index.js
+++ js/index.js
@@ -1,11 +1,71 @@
-const menuToggle = document.querySelector("#menu-toggle");
+/*---------------Mobile navigation script---------------*/
 
-const mobileNavContainer =  document.querySelector('#section2__mobile-nav');
+executeMobilenav();
 
-menuToggle.onclick = function(){ 
+function executeMobilenav(){
 
-    this.classList.toggle("menu-icon-active");
-    mobileNavContainer.classList.toggle('section2__mobile-nav-active')
-    
+        const menuToggle = document.querySelector("#menu-toggle");
+
+        const mobileNavContainer =  document.querySelector('#section2__mobile-nav');
+
+        menuToggle.onclick = function(){ 
+
+            this.classList.toggle("menu-icon-active");
+            
+            mobileNavContainer.classList.toggle('section2__mobile-nav-active');
+            
+        }
+
+    }
+/*---------------------------------------------------*/
+
+
+
+
+
+
+/*-----------------Gallery script--------------------*/
+
+executeGallery();
+
+function executeGallery() {
+
+        const galBtn = document.getElementsByClassName("section7__btn")[0];
+        
+        let overlay = document.getElementById('overlay');
+
+        let arrowLeft = document.getElementById('left-arow');
+
+        let bg = document.getElementsByClassName('gallery__image')[0];
+        
+        let arrowRight =document.getElementById('right-arrow');
+        
+        let closeOver = document.getElementById("close");
+
+        galBtn.onclick = function() {
+            
+            overlay.classList.add("section7__overlay");
+            closeOver.style.display = "block";
+        }
+
+        closeOver.onclick = function() {
+
+            overlay.classList.remove("section7__overlay");
+
+        }
+
+        window.onclick = function(event) {
+            if(event.target == overlay) {
+
+            overlay.classList.remove("section7__overlay");
+
+            }
+        }
+
+    if(overlay.contains("section7__overlay")) {
+
+        closeOver.style.display = "none";
+    }
 }
 
+/*--------------------------------------------------------------------------------*/
