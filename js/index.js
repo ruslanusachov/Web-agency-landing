60d25f191602b1ea21dccfc677b625ef3649091b
Ruslan <ruslanusachov2@gmail.com>
2020-09-10 05:55:34 +0300

    animated section8

--- js/index.js
+++ js/index.js
@@ -123,3 +123,56 @@ function executeGallery() {
 }
 
 
+window.addEventListener('scroll', executeAnimation);
+executeAnimation();
+
+function executeAnimation() {
+
+    let section8 = document.getElementsByClassName('section8__imgSection')[0];
+
+    
+
+    let section8Position = {
+
+        top: window.pageYOffset + section8.getBoundingClientRect().top,
+        left: window.pageXOffset + section8.getBoundingClientRect().left,
+        right: window.pageXOffset + section8.getBoundingClientRect().right,
+        bottom: window.pageYOffset + section8.getBoundingClientRect().bottom,
+        
+    };
+
+
+    let windowPosition = {
+
+        top: pageYOffset,
+        left: pageXOffset,
+        right: pageXOffset + document.documentElement.clientWidth,
+        bottom: pageYOffset + document.documentElement.clientHeight,
+    };
+
+
+    if(section8Position.top < windowPosition.bottom && section8Position.left < windowPosition.right && section8Position.bottom > windowPosition.top && section8Position.right > windowPosition.left) {
+
+        animate();
+    }
+    
+    
+
+    function animate(){
+
+        let iphone = document.getElementsByClassName('section8__iphone')[0];
+
+        let ipad = document.getElementsByClassName('section8__ipad')[0];
+
+        let macbook = document.getElementsByClassName('section8__macbook')[0];
+
+        iphone.classList.toggle('section8__iphone-seen');
+
+        ipad.classList.toggle('section8__ipad-seen');
+
+        macbook.classList.toggle('section8__macbook-seen');
+
+    };
+
+
+};
